479e9eb4a450b8aa5921fe8fb46431e4c629fbf2
Sanlo Zhang <940072028@qq.com>
2021-08-26 15:03:27 +0800

    reorginize config admin

--- typeidea/config/admin.py
+++ typeidea/config/admin.py
@@ -2,23 +2,16 @@ from django.contrib import admin
 
 from .models import Link, SideBar
 from typeidea.custom_site import custom_site
+from typeidea.base_admin import BaseOwnerAdmin
 
 
 @admin.register(Link, site=custom_site)
-class LinkAdmin(admin.ModelAdmin):
+class LinkAdmin(BaseOwnerAdmin):
     list_display = ('title', 'href', 'status', 'weight', 'created_time')
     fields = ('title', 'href', 'status', 'weight')
 
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(LinkAdmin, self).save_model(request, obj, form, change)
-
 
 @admin.register(SideBar, site=custom_site)
-class SideBarAdmin(admin.ModelAdmin):
+class SideBarAdmin(BaseOwnerAdmin):
     list_display = ('title', 'display_type', 'content', 'created_time')
     fields = ('title', 'display_type', 'content')
-
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super(SideBarAdmin, self).save_model(request, obj, form, change)
