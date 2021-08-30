4ec5fb4547027c0b504574aacb49b69a622f8a92
Sanlo Zhang <940072028@qq.com>
2021-08-30 20:37:46 +0800

    UI Styled using bootstrap

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404
 
 from .models import Post, Tag, Category
 from config.models import SideBar
@@ -62,24 +62,3 @@ class PostDetailView(CommonViewMixin, DetailView):
     context_object_name = 'post'
     template_name = 'blog/detail.html'
     pk_url_kwarg = 'post_id'
-
-
-def post_list(request, category_id=None, tag_id=None):
-    tag = None
-    category = None
-
-    if tag_id:
-        post_list, tag = Post.get_by_tag(tag_id)
-    elif category_id:
-        post_list, category = Post.get_by_category(category_id)
-    else:
-        post_list = Post.latest_posts()
-
-    context = {
-        'category': category,
-        'tag': tag,
-        'post_list': post_list,
-        'sidebars': SideBar.get_all(),
-    }
-    context.update(Category.get_navs())
-    return render(request, 'blog/list.html', context=context)
