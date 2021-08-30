52e38a888c5bd6e83ed3db82ceb11c8a5384e30d
Sanlo Zhang <940072028@qq.com>
2021-08-30 23:33:45 +0800

    Add friend links

--- typeidea/config/models.py
+++ typeidea/config/models.py
@@ -21,7 +21,7 @@ class Link(models.Model):
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
     def __str__(self):
-        return self.name
+        return self.title
 
     class Meta:
         verbose_name = verbose_name_plural = '友链'
--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, re_path
 from blog.views import (
     IndexView, CategoryView, TagView, PostDetailView, SearchView, AuthorView
 )
-from config.views import links
+from config.views import LinkListView
 from .custom_site import custom_site
 
 
@@ -32,7 +32,7 @@ urlpatterns = [
             PostDetailView.as_view(), name='post-detail'),
     re_path(r'^author/(?P<owner_id>\d+)/$',
             AuthorView.as_view(), name='author'),
-    path('links/', links, name='links'),
+    path('links/', LinkListView.as_view(), name='links'),
     path('search/', SearchView.as_view(), name='search'),
     path('super_admin/', admin.site.urls, name='super-admin'),
     path('admin/', custom_site.urls, name='admin'),
